217c94816836ed40512925ca2ce086fc65ed6f67
cerramaximiliano <cerramaximiliano@gmail.com>
2023-12-02 20:08:14 -0300

    MongoDB and Mongoose 1/12

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mongoose  = require('mongoose');
 
 
-mongoose.connect(env.process.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
